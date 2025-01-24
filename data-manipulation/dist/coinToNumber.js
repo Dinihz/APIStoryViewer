399dd8de10dd782d0dd48c87e1698989581eb5ec
Lucas Souza Diniz <123830292+Dinihz@users.noreply.github.com>
2025-01-23 21:39:08 -0300

    remove: delete data-manipulation/dist directory

--- data-manipulation/dist/coinToNumber.js
+++
@@ -1,6 +0,0 @@
-export default function coinToNumber(coin) {
-    const number = Number(coin.replaceAll(".", "").replace(",", "."));
-    console.log(`Converting coin: ${coin} to number: ${number}`);
-    return isNaN(number) ? null : number;
-}
-//# sourceMappingURL=coinToNumber.js.map
--- data-manipulation/dist/stringToDate.js
+++
@@ -1,7 +0,0 @@
-export default function stringToDate(text) {
-    const [date, time] = text.split(" ");
-    const [day, month, year] = date.split("/").map(Number);
-    const [hour, minute] = time.split(":").map(Number);
-    return new Date(year, month - 1, day, hour, minute);
-}
-//# sourceMappingURL=stringToDate.js.map
